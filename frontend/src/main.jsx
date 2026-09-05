a954a69c49253edf8fd7585fc401870dfba7e6fb
brunobdantas <brunobdantas@gmail.com>
2026-09-04 23:04:02 -0300

    Switch frontend to UX 5.2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App510.jsx'
+import App from './App520.jsx'
 import './styles.css'
 import './release510.css'
+import './release520.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
