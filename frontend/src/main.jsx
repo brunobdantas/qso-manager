52f01556de70ced834dad608f2e0ee6e57544529
brunobdantas <brunobdantas@gmail.com>
2026-09-11 00:20:11 -0300

    v8: activate online cockpit frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App700.jsx'
+import App from './App800.jsx'
 import './styles.css'
 import './release510.css'
 import './release520.css'
 import './release600.css'
 import './release700.css'
+import './release800.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
