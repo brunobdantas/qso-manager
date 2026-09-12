792d690a8d90aa9ef4e31307998484c81725a98a
brunobdantas <brunobdantas@gmail.com>
2026-09-12 00:30:14 -0300

    v9: make the unified UX the only desktop entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App800.jsx'
-import './styles.css'
-import './release510.css'
-import './release520.css'
-import './release600.css'
-import './release700.css'
-import './release800.css'
+import App from './App900.jsx'
+import './unified900.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
