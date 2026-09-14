576498f3aadb8c67ec52066b47021fa4f1fc81b0
brunobdantas <brunobdantas@gmail.com>
2026-09-14 00:45:14 -0300

    release: show production version in desktop UI

--- frontend/src/App900.jsx
+++ frontend/src/App900.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from 'react'
 
-const VERSION = '9.0.0'
+const VERSION = '1.0.0'
 const NAV = [
   ['overview','Visão geral','⌂'],
   ['log','Log','◎'],
