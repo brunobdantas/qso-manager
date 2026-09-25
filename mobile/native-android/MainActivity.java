f9eb53e1545373baa9955386e134903c20020011
brunobdantas <brunobdantas@gmail.com>
2026-09-24 23:19:17 -0300

    fix(android): register resilient HTTP plugin

---
+++ mobile/native-android/MainActivity.java
@@ -0,0 +1,12 @@
+package net.pu2bru.qsomanager;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ResilientHttpPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
